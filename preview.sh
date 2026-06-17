206e9413867c7b2537374af9b34dd2c3d85b565a
Daniel Schaefer <git@danielschaefer.me>
2026-06-17 22:25:33 +0800

    Add preview.sh to run locally with docker
    Signed-off-by: Daniel Schaefer <git@danielschaefer.me>

---
+++ preview.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run Jekyll blog locally using Docker
+echo "Starting 'Making Utter Sense' local preview..."
+echo "Open http://localhost:4000 in your browser"
+
+docker run --rm \
+  -v "$(pwd):/srv/jekyll:Z" \
+  -p 4000:4000 \
+  -it docker.io/jekyll/jekyll:4.2.2 \
+  jekyll serve --watch --force_polling
